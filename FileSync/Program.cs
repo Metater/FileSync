74b700f7d00a3237f24e795d4576318eba421a34
Connor <61102529+Metater@users.noreply.github.com>
2021-09-06 19:06:58 -0400

    Add project

---
+++ FileSync/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using MetaMitStandard;
+using System.IO.Compression;
+using System.IO;
+using System.Threading;
+
+namespace FileSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("[File Sync] No local file path provided properly!");
+                return;
+            }
+            string localPath = args[0];
+            byte[] data = File.ReadAllBytes($@"{Directory.GetCurrentDirectory()}{localPath}");
+            //byte[] data = new byte[]{ 0x00 };
+            byte[] compressed = Compress(data);
+            bool isDone = false;
+
+            MetaMitClient client = new MetaMitClient();
+            client.Connected += (object sender, MetaMitStandard.Client.ConnectedEventArgs e) =>
+            {
+                Console.WriteLine("[File Sync] Connected to server, sending data!");
+                client.Send(compressed);
+            };
+            client.DataSent += (object sender, MetaMitStandard.Client.DataSentEventArgs e) =>
+            {
+                client.Disconnect();
+            };
+            client.Disconnected += (object sender, MetaMitStandard.Client.DisconnectedEventArgs e) =>
+            {
+                isDone = true;
+            };
+            client.Connect("192.168.1.84", 1744);
+            while (!isDone)
+            {
+                client.PollEvents();
+                Thread.Sleep(20);
+            }
+            Console.WriteLine("[File Sync] Done!");
+        }
+
+        public static byte[] Compress(byte[] data)
+        {
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                dstream.Write(data, 0, data.Length);
+            }
+            return output.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            MemoryStream input = new MemoryStream(data);
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+            {
+                dstream.CopyTo(output);
+            }
+            return output.ToArray();
+        }
+    }
+}
---
+++ FileSyncServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading;
+using MetaMitStandard;
+
+namespace FileSyncServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("No local file output name provided properly!");
+                return;
+            }
+            string outputFileName = args[0];
+            MetaMitServer server = new MetaMitServer(1744, 100, System.Net.IPAddress.Parse("192.168.1.84"));
+            server.ClientConnected += (object sender, MetaMitStandard.Server.ClientConnectedEventArgs e) =>
+            {
+                Console.WriteLine("Client Connected: \n" + "\tGuid: " + e.guid + "\n" + "\tEndpoint: " + e.ep);
+            };
+            server.ClientDisconnected += (object sender, MetaMitStandard.Server.ClientDisconnectedEventArgs e) =>
+            {
+                Console.WriteLine("Client Disconnected: \n" + "\tGuid: " + e.guid);
+            };
+            server.DataReceived += (object sender, MetaMitStandard.Server.DataReceivedEventArgs e) =>
+            {
+                Console.WriteLine("Received file of size " + e.data.Length + " bytes");
+                byte[] data = Decompress(e.data);
+                File.WriteAllBytes(Directory.GetCurrentDirectory() + "/" + outputFileName, data);
+            };
+            server.Start();
+            Console.WriteLine("Listening on: " + server.Ep);
+            while (!Console.KeyAvailable)
+            {
+                server.PollEvents();
+                Thread.Sleep(5);
+            }
+        }
+
+        public static byte[] Compress(byte[] data)
+        {
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                dstream.Write(data, 0, data.Length);
+            }
+            return output.ToArray();
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            MemoryStream input = new MemoryStream(data);
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+            {
+                dstream.CopyTo(output);
+            }
+            return output.ToArray();
+        }
+
+    }
+}
